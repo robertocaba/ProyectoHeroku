1b84e2fec20d59c51d3a3e33fd5cebe26ed09395
SofiaPinilla <contact@sofiapinilla.com>
2022-02-08 17:15:43 +0100

    some names fixed

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,6 +1,5 @@
 const Post = require("../models/Post");
 
-
 const PostController ={
     async create(req,res){
         try {
--- Routes/posts.js
+++ Routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const PostController = require('../controllers/PostController');
 const {authentication} = require('../middlewares/authentication');
-
+ 
 router.post('/',authentication,PostController.create);
 router.put('/:_id',authentication, PostController.update);
 router.delete('/id/:_id',authentication,PostController.delete);
